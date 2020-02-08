4e11e613bc4affb60abb86a4f9d9aaf9cb2105a4
Cesaar Valdez <valdezcervera.info@gmail.com>
2020-02-08 19:22:52 +0100

    Remove console.log

--- src/app/components/tree-add-node/tree-add-node.component.ts
+++ src/app/components/tree-add-node/tree-add-node.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { ApiClientService } from 'src/app/api-client.service';
-import { TNodeChild, TNodeParent } from '../../data-types/node'
+import { TNodeChild, TNodeParent } from '../../data-types/node';
 import { Item } from '../Models/Item';
-import { parse, stringify } from 'querystring';
 
 @Component({
   selector: 'app-tree-add-node',
@@ -13,32 +12,31 @@ export class TreeAddNodeComponent implements OnInit {
 
   constructor(private apiClinet: ApiClientService) { }
 
-  @Output() 
-  nodeAdd: EventEmitter<any> = new EventEmitter<any>()
-  @Input() 
-  tree: TNodeParent
+  @Output()
+  nodeAdd: EventEmitter<any> = new EventEmitter<any>();
   @Input()
-  listItems: Array<TNodeChild>
+  tree: TNodeParent;
   @Input()
-  parentsID 
-  nodeModel: TNodeChild = new Item(null, '', { id: null })
+  listItems: Array<TNodeChild>;
+  @Input()
+  parentsID;
+  nodeModel: TNodeChild = new Item(null, '', { id: null });
 
   ngOnInit() { }
 
   onSubmit = () => {
-      //save model id from this context
-    const modelId: any = this.nodeModel.id
+      // save model id from this context
+    const modelId: any = this.nodeModel.id;
       // get the list item which corresponds to our nodeModel. Note: nodeModel.id was converted
       // to a string by ngModel.
-    const filteredListItem = this.listItems.filter((el) => el.id === parseInt(modelId));
+    const filteredListItem = this.listItems.filter((el) => el.id === parseInt(modelId, 10));
       // assing the proper name to our model for this context.
-    this.nodeModel.name = filteredListItem[0].name
+    this.nodeModel.name = filteredListItem[0].name;
       // add the new node to the tree
     this.apiClinet.addNode(this.nodeModel).subscribe((data: any) => {
-      this.parentsID.push(data.id.toString())
+      this.parentsID.push(data.id.toString());
         // with the modelId from our working context, remove it from the db-listItems
-      this.nodeAdd.emit(modelId)
-    })
+      this.nodeAdd.emit(modelId);
+    });
   }
-
 }
--- src/app/components/tree-controller-component/tree-controller-component.component.ts
+++ src/app/components/tree-controller-component/tree-controller-component.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiClientService } from 'src/app/api-client.service';
-import { TNodeParent } from 'src/app/data-types/node';
+import { TNodeParent, TNodeChild } from 'src/app/data-types/node';
 
 @Component({
   selector: 'app-tree-controller-component',
@@ -12,78 +12,78 @@ export class TreeControllerComponentComponent implements OnInit {
   constructor(private apiClinet: ApiClientService) { }
 
   tree: TNodeParent[];
-  listItems: Array<Object>
-  parentsID = [] 
+  listItems: Array<TNodeChild>;
+  parentsID = [];
 
   treeTraverse(tree) {
-    let res=[]
+    const res = [];
     tree
     ?
     tree.forEach(node => {
-      if (node.children.length === 0) return this.parentsID.push(node.id)
-      this.parentsID.push(node.id)     
-        this.treeTraverse(node.children)
+      if (node.children.length === 0) { return this.parentsID.push(node.id); }
+      this.parentsID.push(node.id);
+      this.treeTraverse(node.children);
     })
     :
-    res.push('no values found')
-    return res
+    res.push('no values found');
+    return res;
   }
 
   ngOnInit() {
     // this.getAllNodes()
     this.apiClinet.getTree().subscribe((data: TNodeParent[]) => {
-      this.tree = data.map((node: TNodeParent) => node)
-      this.treeTraverse(this.tree)
-    })
-    this.getListItems()
+      this.tree = data.map((node: TNodeParent) => node);
+      this.treeTraverse(this.tree);
+    });
+    this.getListItems();
+    console.log('no console please');
   }
 
   getNotification($event) {
-    this.getAllNodes()
-    this.removeListItem($event)
+    this.getAllNodes();
+    this.removeListItem($event);
   }
 
-  //Tree
+  // Tree
   getAllNodes() {
     this.apiClinet.getTree().subscribe((data: TNodeParent[]) => {
-      this.tree = data.map((node: TNodeParent) => node)
-    })
+      this.tree = data.map((node: TNodeParent) => node);
+    });
   }
 
   removeTreeItem(item) {
     this.apiClinet.removeNode(item.id).subscribe((data: any) => {
-      console.log(data)
       data.map((el) => {
-        this.addListItem(el) 
-        this.spliceIdFromList(el.id)
-      })
-      this.getAllNodes()
-    })
+        this.addListItem(el);
+        this.spliceIdFromList(el.id);
+      });
+      this.getAllNodes();
+    });
   }
   // handle ParentID's list
   spliceIdFromList(id) {
-    let i:any  
-  for(i in this.parentsID){
-    if(this.parentsID[i]==id){
-      this.parentsID.splice(i,1);
+    let i: any;
+    for (i in this.parentsID) {
+      if (this.parentsID[i] == id) {
+        this.parentsID.splice(i, 1);
         break;
+        }
       }
-    }
   }
-  //List
+  // List
   getListItems() {
-    this.apiClinet.getList().subscribe((data: Array<Object>) => {
-      this.listItems = data.map( (item: Object) => item)
-    })
+    this.apiClinet.getList().subscribe((data: Array<TNodeChild>) => {
+      this.listItems = data.map( (item: TNodeChild) => item);
+    });
   }
   removeListItem(id) {
     this.apiClinet.removeListItem(id).subscribe(() => {
-      this.getListItems()
-    })
+      this.getListItems();
+    });
   }
   addListItem(item) {
     this.apiClinet.addListItem({name: item.name}).subscribe(() => {
-      this.getListItems()
-    })
+      this.getListItems();
+    });
   }
 }
